441206e03762c475d72dac7976e491c36db9c1ff
Robert-Stefan <robertboss05@hotmail.it>
2021-03-25 14:23:56 +0100

    Add Snack 2 && Snack 3

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-/*Snack 1
+/* Snack 1
 
     Il software deve chiedere per 5 volte all’utente di inserire un numero.
     Il programma stampa la somma di tutti i numeri inseriti.
@@ -25,3 +25,56 @@ console.log(somma);*/
     somma += utente;
 }
 console.log(somma);*/
+
+
+/* Snack 2
+
+Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+
+*/
+
+/*var utente = parseInt(prompt('Inserisci un numero: '));
+somma = 1;
+
+if (utente % 2 != 0) {
+    console.log(utente);
+} else {
+    totale = somma + utente;
+    console.log(totale);
+}*/
+
+
+/* Snack 3
+ 
+Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.    
+
+*/
+
+/*var nomi = ['Luca', 'Antonio', 'Paolo', 'Sempronio'];
+console.log(nomi);
+var cognomi = ['Stefano', 'Petrucci', 'Pincopallino', 'Corrido'];
+console.log(cognomi);
+
+lista3 = [];
+counter = 1;
+max = 4;
+
+while ( counter < max ) {
+    var lista = nomi[Math.floor(Math.random() * nomi.length)];
+    
+    var lista2 = cognomi[Math.floor(Math.random() * cognomi.length)];
+    
+    var newlista3 = lista + ' ' + lista2;
+
+    lista3.push(newlista3);
+
+    counter++;
+}
+console.log(lista3);*/
+
+
+/** Snack 4
+
+Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+*/
